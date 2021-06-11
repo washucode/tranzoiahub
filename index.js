853ae521e19e90c984fb371ec98d6a39671b30c1
estherwashu <esther@bitlipa.africa>
2021-06-11 19:35:27 +0300

    Add routes to render view pages

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const port = 3000;
 
 
 app.set("view engine","pug");
+app.use(express.static( 'assets'));
 
 
 app.get('/', function (req, res) {
   res.render("landing");
 });
+
+app.get('/about', function (req, res) {
+    res.render("about");
+  });
+
+  app.get('/programs', function (req, res) {
+    res.render("programs");
+  });
+  app.get('/joinus', function (req, res) {
+    res.render("joinus");
+  });
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
