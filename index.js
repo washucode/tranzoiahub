a06f98f668dd4cba28e49d73d04c008ea59f85ce
washucode <estherw8525@gmail.com>
2021-07-16 01:20:15 +0300

    add dotenv dependance

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 4000;
 const db = require('./queries')
+const nodemailer = require('nodemailer')
+ const { MAIL_USER,  MAIL_PASS } = require('./config');
+ 
 
 app.use(bodyParser.json())
 app.use(
@@ -20,6 +23,7 @@ app.use(express.static( 'assets'));
 
 
 app.get('/', function (req, res) {
+  
   res.render("landing");
 });
 
@@ -41,3 +45,40 @@ app.get('/about', function (req, res) {
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
+
+
+
+app.post('/contactus', (req, res) => {
+
+  // Instantiate the SMTP server
+  const smtpTrans = nodemailer.createTransport({
+    host: 'tranzoiahub.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: MAIL_USER,
+      pass: MAIL_PASS
+    }
+  })
+
+  // Specify what the email will look like
+  const mailOpts = {
+    from: `${req.body.email}`, // This is ignored by Gmail
+    to: MAIL_USER,
+    subject: 'New message from contact form at tranzoiahub.com, the subject is' ,
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  }
+
+  // Attempt to send the email
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      console.log(error)
+      res.render('contactfail') // Show a page indicating failure
+    }
+    else {
+      res.render('contactsuccess') // Show a page indicating success
+    }
+  })
+})
+
+
