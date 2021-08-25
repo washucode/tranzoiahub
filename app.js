668fdb931074f6f5805bfbcc09aad8971fc74640
washucode <estherw8525@gmail.com>
2021-08-25 13:25:06 +0300

    rename index.js

--- app.js
+++ app.js
@@ -81,4 +81,15 @@ app.post('/contactus', (req, res) => {
   })
 })
 
-module.exports = app;
+var server = app.listen()
+
+
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var message = 'It works!\n',
+//         version = 'NodeJS ' + process.versions.node + '\n',
+//         response = [message, version].join('\n');
+//     res.end(response);
+// });
+// server.listen();
